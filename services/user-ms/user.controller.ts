28fbd4762de7dd3024d938fbf4c4b4598bb7b044
Venkatesh B <venky88.iiitb@gmail.com>
2025-06-12 22:11:51 +0530

    cf commit

---
+++ services/user-ms/user.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() user: Partial<User>): Promise<User> {
+    return this.userService.create(user);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() user: Partial<User>): Promise<User> {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<void> {
+    return this.userService.remove(id);
+  }
+}
